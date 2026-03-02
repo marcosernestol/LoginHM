f96a5feb486a4621ab92ac1798a8db261f2b0ef3
Marcos Lozano <desarrollo@itsmarts.com.mx>
2026-03-02 13:28:41 -0600

    Sigio probando

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,20 +12,24 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('LoginHM');
 
-  constructor() {
-
-    alert('LoginHM');
-
-  }
-}
+  constructor() {};
 
 
 
+
+    guardar_formulario(){  alert('LoginHM'); };
+
+
+ }
+
+
+
+/*
 'use strict' // Para que se use la notación estrícta
 
 
 // La opción > ng serve -o   (Funciona solo para un proyecto ANGULAR, Desde una terminal “node” o “cmd”)
-// Pero Para DESARROLLO ( en proyectos que no usan ANGULAR), 
+// Pero Para DESARROLLO ( en proyectos que no usan ANGULAR),
 //      Está muy padre la librería "LIVE SERVER" - una vez instalada solo presionar opción "Go Live"
 
 window.addEventListener('load',function(){
@@ -35,9 +39,9 @@ window.addEventListener('load',function(){
    console.log("Inicio OK, Pagina Inicial");
    alert('LoginHM !!!!');         // ALERTA
 
-// XXXXXXXXXXXX   CON ESTO ESTARIA CONSULTANDO UN PRIMER WS .....   ( Apoco SI TILIN !!! ???  )   XXXXXXXXXXXXXXXXXX
+ // XXXXXXXXXXXX   CON ESTO ESTARIA CONSULTANDO UN PRIMER WS .....   ( Apoco SI TILIN !!! ???  )   XXXXXXXXXXXXXXXXXX
 // xxxxxxxxxxxxx    ASI DE SIMPLE ,  CONSULTANDO MI PRIMER WS ,   POR FIN !!!! SI FUNCIONÓ !!!   XXXXXXXXXXXXXXXXXX
-    var usuarios_desde_WS_RegistroPersonas = []; 
+    var usuarios_desde_WS_RegistroPersonas = [];
     fetch('http://localhost:5999/api/RegistroPersona')
     .then(data =>  data.json())
     .then(data => {
@@ -56,21 +60,21 @@ window.addEventListener('load',function(){
 var miFormulario = document.querySelector("#primerFormulario");
 
 
-// Luego generamos un evento 
+// Luego generamos un evento
  miFormulario.addEventListener('submit',function(){
     console.log("Entra Submit");
 
     var varNombreUnico = document.querySelector("#inputNombreUnico").value;
     var varCorreoElectronico = document.querySelector("#inputCorreoElectronico").value;
     var varPassword = document.querySelector("#inputPassword").value;
-  
+
 
      // En estas variables se capturan los datos y lo comprobaremos asi:
      console.log(varNombreUnico , varCorreoElectronico, varPassword );
 
         // AHORA VAMOS A VALIDAR los datos de entrada, por ejemplo :
         // Que el campo se llene y NO esté vacío y que NO tenga espacios al inicio y fin
-     
+
         // Con el siguiente if es fácil de validar y No nos deja pasar hasta que el dato dea válido
         if(varNombreUnico.trim() == null || varNombreUnico.trim().length == 0){
 			alert("El NombreUnico no es válido");
@@ -96,10 +100,13 @@ var miFormulario = document.querySelector("#primerFormulario");
 			return false;
 		}else{
             document.querySelector("#errorPassword").style.display = "none" ;
-        }
-
- }); 
+        } */
 
 
 
-});  // Se cierra el evento 'load'
+
+  // Se cierra el evento 'load'
+
+
+
+
